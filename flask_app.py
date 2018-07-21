631a2361d9c3a8bc7e06489e69afbc4e7cf9d2f0
Maureen Slavin <maureen.slavin@q2ebanking.com>
2018-07-21 13:52:03 -0500

    Initial commit of working code for Weeks 6 and 7

---
+++ flask_app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def main():
+    # found in ../templates/
+    return render_template("main_page.html")
+
+@app.route('/process_inputs', methods=['POST'])
+def process_inputs():
+    name = request.form.get('input_name', '')
+    dropdown = request.form.get('input_dropdown', '')
+    select = request.form.get('input_select', '')
+    freeform = request.form.get('input_freeform', '')
+    return render_template("main_page.html", input_data=dropdown,
+                           output="You're a wizard %s." % name)
+
+
+if __name__ == '__main__':
+    app.run()
